03f0f822cc8bf54f049c1c8d31dc188dc77241dd
elizabete <elizabetereiss@gmail.com>
2017-06-04 22:31:22 -0300

    first commit

---
+++ src/pokemon/Mapa.java
@@ -0,0 +1,102 @@
+package pokemon;
+
+import jeops.AbstractKnowledgeBase;
+
+public class Mapa extends jeops.AbstractRuleBase{
+
+	public Mapa(AbstractKnowledgeBase knowledgeBase) {
+		super(knowledgeBase);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean checkCondForDeclaration(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean checkCondition(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean checkConditionsOnlyOf(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Class getDeclaredClass(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDeclaredClassName(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getDeclaredIdentifiers(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getNumberOfConditions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getNumberOfDeclarations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getNumberOfRules() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getObject(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] getObjects(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getRuleNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void internalFireRule(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setObject(int arg0, int arg1, Object arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setObjects(int arg0, Object[] arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
